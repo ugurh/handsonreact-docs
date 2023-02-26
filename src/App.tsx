3ecf59af43338c8980a741481776b9aa8d2fd05c
harun ugur <ugur.harun@yandex.com>
2023-02-26 19:08:32 +0300

    Create a Navigation Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import "./App.css";
-import Routing from "./projects/js/Routing";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import Home from "./projects/js/Home";
+import ProjectPage from "./projects/js/ProjectPage";
 
 function App() {
   return (
-    <div>
-      <Routing />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<ProjectPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
